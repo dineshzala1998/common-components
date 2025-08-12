c30ca10afe47ec0072eaad8af8de206f8fddd87e
dineshm.cmarix <dinesh.makwana@cmarix.com>
2025-08-12 19:29:15 +0530

    input box design creat

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,9 @@
 
+
+
 $(document).ready(function () {
+
+    
     /* Custom FAQ Accordion Js Start */
     $('#CustomAccordion .faq-item .faq-item-header').on('click', function () {
         const $this = $(this);
@@ -16,13 +20,48 @@ $(document).ready(function () {
     });
     /* Custom FAQ Accordion Js End */
 
+    /*========================================================================================================================================*/
+
     // Initialize Select2 SelectPicker
     $('.select2-selectpicker').select2({
         tags: "true",
         // minimumResultsForSearch: Infinity, // disables search
     });
-});
 
+
+    /*========================================================================================================================================*/
+
+    // Label Animated Js start here...
+    $('.label-animated .form-control').each(function () {
+        if ($(this).val() !== '') {
+            $(this).addClass('filled');
+        }
+    
+        $(this).on('input', function () {
+            if ($(this).val() !== '') {
+            $(this).addClass('filled');
+            } else {
+            $(this).removeClass('filled');
+            }
+        });
+    
+        $(this).on('focus', function () {
+            $(this).addClass('filled');
+        });
+    
+        $(this).on('blur', function () {
+            if ($(this).val() === '') {
+            $(this).removeClass('filled');
+            }
+        });
+    });
+    // Label Animated Js End
+
+    /*========================================================================================================================================*/
+});
+/*========================================================================================================================================*/
+
+// Country Code & Flag Select2 Js Start here.......
 function formatCountry(option) {
   if (!option.id) return option.text;
   var flagUrl = $(option.element).data('flag');
@@ -38,5 +77,35 @@ $(document).ready(function () {
     templateSelection: formatCountry
   });
 });
+// Country Code & Flag Select2 Js End
+
+/*========================================================================================================================================*/
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Input Type Password Hide show icon Js Start here........ 
+    const toggles = document.querySelectorAll(".togglePassword");
+    toggles.forEach(function(toggle) {
+        toggle.addEventListener("click", function (e) {
+            e.preventDefault();
+            const passwordField = toggle.nextElementSibling;
+            const eyeIcon = toggle.querySelector("img");
+
+            const isHidden = passwordField.type === "password";
+            passwordField.type = isHidden ? "text" : "password";
+            eyeIcon.src = isHidden ? "assets/images/eye.svg" : "assets/images/eye-slash.svg";
+        });
+    });
+
+    // Input Tel Js Start here........ 
+    const input = document.querySelector("#phone");
+    window.intlTelInput(input, {
+        initialCountry: "in", // default India
+        nationalMode: false,  // shows country code
+        separateDialCode: true, // show dial code separately
+        dropdownContainer: document.body, // keeps dropdown in body
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js"
+    });
+});
+
 
 
